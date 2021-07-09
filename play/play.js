9d049f5db183720d077904df47458616f50c3de6
ref1821 <ref1821@gmail.com>
2021-07-09 14:32:03 -0500

    hasta aqui funciona 9/7

--- play/play.js
+++ play/play.js
@@ -9,8 +9,6 @@ function randint(min,max){
   return Math.floor(Math.random()*(1+max-min)+min);
 }
 
-
-
 const as = 1;
 const dos = 2;
 const tres = 3;
@@ -27,37 +25,54 @@ const rey = 10;
 
 var num = new Array(as, dos, tres, cuatro, cinco, seis, siete, ocho ,nueve, diez, joto, reina, rey);
 
-
 var dnum = num[Math.floor(Math.random()*num.length)];
 var dnum2 = num[Math.floor(Math.random()*num.length)];
 
-
+ var totalUno = dnum + dnum2;
 
 console.log(dnum);
 console.log(dnum2);
 
-function yes(){
+function nothing(){
   return;
 }
 
-
-const tex = document.querySelector('#text');
-tex.textContent = "your numbers are  "+ dnum +  " & " +dnum2;
-
-totalUno = dnum + dnum2;
-
-y = document.getElementById("yes").addEventListener("click", () =>{
-  var dnum3 = num[Math.floor(Math.random()*num.length)];
+function yes(){
+  let dnum3 = num[Math.floor(Math.random()*num.length)];
   var n3 = document.createElement("p");
   n3.innerHTML = "new number is = "+ dnum3;
   document.getElementById("div").appendChild(n3);
   console.log(dnum3)
   totalUno += dnum3
   console.log(totalUno)
+  if(totalUno>21){
+    var lose = document.createElement("p");
+    lose.innerHTML = "you lost"
+    document.getElementById("div").appendChild(lose);
+    document.getElementById("yes").addEventListener("click", () => {
+      nothing();
+    });
+  }
+
+}
+
+const tex = document.querySelector('#text');
+tex.textContent = "your numbers are  "+ dnum +  " & " +dnum2;
+
+
+
+document.getElementById("yes").addEventListener("click", () => {
+  yes()
 });
 
+document.getElementById("no").addEventListener("click", () => {
+  var no = document.createElement("p");
+  no.innerHTML = "your final number is =" + totalUno
+  document.getElementById("div").appendChild(no);
+})
 
-n = document.getElementById("no").addEventListener("click", () =>{
+document.getElementById("fg").addEventListener('click', () => {
   var nf = document.createElement("p");
-  nf.innerHTML = "your final number is =" + dnum3
+  nf.innerHTML = "your final number is =" + totalUno
+  document.getElementById("div").appendChild(nf);
 })
